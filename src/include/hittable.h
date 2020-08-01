2486d598ed73a69e9dcbcb534f34379184a5f02a
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-01 18:12:33 +0200

    Add world spheres and fix some syntax errors

--- src/include/hittable.h
+++ src/include/hittable.h
@@ -21,6 +21,6 @@ class hittable
 {
 public:
     virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const = 0;
-}
+};
 
 #endif
--- src/include/hittable_list.h
+++ src/include/hittable_list.h
@@ -22,10 +22,9 @@ public:
 
 public:
     std::vector<shared_ptr<hittable>> objects;
-}
+};
 
-bool
-hittable_list::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+bool hittable_list::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
 {
     hit_record temp_rec;
     bool hit_anything = false;
