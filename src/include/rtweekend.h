f92662166f544be602b7318457f9b743680f8e9a
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-02 17:34:38 +0200

    Implement antialiasing

--- src/include/rtweekend.h
+++ src/include/rtweekend.h
@@ -23,10 +23,10 @@ inline double degrees_to_radians(double degrees)
     return degrees * pi / 180.0;
 }
 
-inline double random_double()
-{
-    return rand() / (RAND_MAX + 1.0);
-}
+// inline double random_double()
+// {
+//     return rand() / (RAND_MAX + 1.0);
+// }
 
 // inline double random_double(double min, double max)
 // {
@@ -38,6 +38,15 @@ inline double random_double()
     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
     static std::mt19937 generator;
     return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max)
+{
+    if (x < min)
+        return min;
+    if (x > max)
+        return max;
+    return x;
 }
 
 // Common Headers
