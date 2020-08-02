39f5755249b026cd183c72331d5d191fd87a7d9f
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-02 17:19:08 +0200

    Add random utility functions

--- src/include/rtweekend.h
+++ src/include/rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <random>
 #include <cstdlib>
 #include <limits>
 #include <memory>
@@ -10,13 +11,36 @@ using std::make_shared;
 using std::shared_ptr;
 using std::sqrt;
 
+// Constants
+
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
+
+// Utility functions
 
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
 }
+
+inline double random_double()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// inline double random_double(double min, double max)
+// {
+//     return min + (max - min) * random_double();
+// }
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+// Common Headers
 
 #include "ray.h"
 #include "vec3.h"
