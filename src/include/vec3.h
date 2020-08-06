8862a14e64e383e842fdaea4a14ecc257f6f4d7c
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-06 09:25:42 +0200

    Add fuzzy distorsion to metal materials

--- src/include/vec3.h
+++ src/include/vec3.h
@@ -11,16 +11,28 @@ using std::sqrt;
 class vec3
 {
 public:
-    vec3() : e{0, 0, 0} {}
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    vec3() : e{ 0, 0, 0 } {}
+    vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+    double x() const {
+        return e[0];
+    }
+    double y() const {
+        return e[1];
+    }
+    double z() const {
+        return e[2];
+    }
 
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    double &operator[](int i) { return e[i]; }
+    vec3 operator-() const {
+        return vec3(-e[0], -e[1], -e[2]);
+    }
+    double operator[](int i) const {
+        return e[i];
+    }
+    double &operator[](int i) {
+        return e[i];
+    }
 
     vec3 &operator+=(const vec3 &v)
     {
@@ -116,8 +128,8 @@ inline double dot(const vec3 &u, const vec3 &v)
 inline vec3 cross(const vec3 &u, const vec3 &v)
 {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(vec3 v)
@@ -125,16 +137,16 @@ inline vec3 unit_vector(vec3 v)
     return v / v.length();
 }
 
-// vec3 random_in_unit_sphere()
-// {
-//     while (true)
-//     {
-//         auto p = vec3::random(-1, 1);
-//         if (p.length_squared() >= 1)
-//             continue;
-//         return p;
-//     }
-// }
+vec3 random_in_unit_sphere()
+{
+    while (true)
+    {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1)
+            continue;
+        return p;
+    }
+}
 
 vec3 random_unit_vector()
 {
