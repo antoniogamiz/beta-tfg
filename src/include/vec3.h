bcb3f5143cd188ae93238d7a6e43c93ca23cedeb
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-06 11:50:20 +0200

    Add final world distribution

--- src/include/vec3.h
+++ src/include/vec3.h
@@ -156,4 +156,12 @@ vec3 random_unit_vector()
     return vec3(r * cos(a), r * sin(a), z);
 }
 
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
 #endif
