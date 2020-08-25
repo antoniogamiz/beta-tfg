564625780dee1d7be2c6fda39cde910c18e9e5ba
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-25 09:16:56 +0200

    Improve progress script

--- show_progress.sh
+++ show_progress.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 function progress {
-    echo  " $1/$2"
+    echo  "$1/$2"
 }
+
+[ ! -d "progress" ] && echo "Ray tracer is not running!" && exit
 
 PROGRESS_DIR=progress
 
 total_iterations=0
 current_iterations=0
 
-for entry in "$PROGRESS_DIR"/*
+while :
 do
-  local_progress=($(awk '/./{line=$0} END{print line}' $entry))
-  current_iterations=$(($current_iterations+${local_progress[0]}))
-  total_iterations=$(($total_iterations+${local_progress[1]}))
+  for entry in "$PROGRESS_DIR"/*
+  do
+    local_progress=($(awk '/./{line=$0} END{print line}' $entry))
+    current_iterations=$(($current_iterations + ${local_progress[0]}))
+    total_iterations=$(($total_iterations + ${local_progress[1]}))
+  done
+
+
+  clear
+  progress $current_iterations $total_iterations
+	sleep 1
 done
 
-progress $current_iterations $total_iterations
