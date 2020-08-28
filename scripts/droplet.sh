6fe674f548419a44b33f8a746121d4496e588872
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-28 12:56:06 +0200

    Fix slug size name

--- scripts/droplet.sh
+++ scripts/droplet.sh
@@ -3,7 +3,7 @@
 key_md5="$(ssh-keygen -l -E md5 -f ~/.ssh/id_rsa.pub | cut -d ' ' -f2)"
 key_md5="${key_md5##MD5:}"
 
-doctl compute droplet create RayTracer --image ubuntu-20-04-x64 --size c2 --region nyc1 --ssh-keys $key_md5
+doctl compute droplet create RayTracer --image ubuntu-20-04-x64 --size c-2 --region nyc1 --ssh-keys $key_md5
 
 # to disable known_hosts warning (not recommended): https://superuser.com/a/853144/1161534
 # Warning: Using the doctl Snap? Grant access to the ssh-keys interface with this command: sudo snap connect doctl:ssh-keys
