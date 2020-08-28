78e5a4e6f3693ff019bbb0aceba092c75228cfac
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-08-28 13:14:45 +0200

    Fix typo in nohup command

--- scripts/run-raytracer.sh
+++ scripts/run-raytracer.sh
@@ -16,3 +16,4 @@ pnmtopng test.ppm > result.png
 echo "Execution time: $(($end-$begin))" | mutt -a "./result.png" -s "Run $(date)" -- antoniogamiz10@gmail.com
 
 # doctl compute droplet delete RayTracer
+# to check progress: doctl compute ssh RayTracer --ssh-command 'cd repo && bash show_progress.sh'
