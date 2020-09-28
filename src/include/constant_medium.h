ad9bc6dab1e1287bec61a00d6fb76b1e18e3e990
antoniogamiz <antoniogamiz10@gmail.com>
2020-09-28 12:12:15 +0200

    feat: add cornell box with smoke, refs #7

--- src/include/constant_medium.h
+++ src/include/constant_medium.h
@@ -10,7 +10,7 @@
 class constant_medium : public hittable
 {
 public:
-    constant_medium(shared_ptr<hittable> b, double d, shared_ptr<texture> a) : boundary(b) neg_inv_density(-1 / d), phase_function(make_shared<isotropic>(a)) {}
+    constant_medium(shared_ptr<hittable> b, double d, shared_ptr<texture> a) : boundary(b), neg_inv_density(-1 / d), phase_function(make_shared<isotropic>(a)) {}
     constant_medium(shared_ptr<hittable> b, double d, color c) : boundary(b), neg_inv_density(-1 / d), phase_function(make_shared<isotropic>(c)) {}
 
     virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
