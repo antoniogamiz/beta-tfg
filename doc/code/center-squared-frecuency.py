f4152809cb1c294f24da5e441ee4e2a2faa47a86
Antonio Gamiz <antoniogamiz10@gmail.com>
2020-10-13 21:54:45 +0200

    docs(code): plot of middle-square method (#23)

--- doc/code/center-squared-frecuency.py
+++ doc/code/center-squared-frecuency.py
@@ -1,4 +1,14 @@
 import matplotlib.pyplot as plt
+import collections
+
+plt.style.use('seaborn-whitegrid')
+
+
+limit = 100000
+n = len(str(limit))
+lower_bound = int(n/2-n/4)
+upper_bound = int(n/2+n/4)
+
 
 def center_squared_method(seed):
     generated = set()
@@ -7,14 +17,26 @@ def center_squared_method(seed):
     while number not in generated:
         counter += 1
         generated.add(number)
-        number = int(str(number * number).zfill(8)[2:6])
+        number = int(str(number * number).zfill(n)[lower_bound:upper_bound])
     return counter
 
 
 frequency = []
-for i in range(1, 1001):
+for i in range(1, limit):
     frequency.append(center_squared_method(i))
 
-plt.plot(range(1,1001), frequency)
-plt.ylabel('Periodo')
+# plt.plot(range(1, limit), frequency, 'o', color='black')
+# plt.ylabel('Periodo')
+# plt.show()
+
+# ============ BARRAS ==================
+
+c = collections.Counter(frequency)
+c = sorted(c.items())
+months_num = [i[0] for i in c]
+freq = [i[1] for i in c]
+
+plt.bar(months_num, freq)
+plt.ylabel("Frecuencia")
+plt.xlabel("Periodo")
 plt.show()
